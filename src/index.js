14d3adcb47f7944988339987aaf4be06b8270a30
PercyPham <hungpm.percy@gmail.com>
2019-11-09 17:11:38 +0700

    made simple connection between client and server via socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("public"));
-
+// Do this since app.listen will return an instance of http.Server
+//  not the Express instance
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-module.exports = server;
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", socket => {
+  console.log(`got a socket connection with id: ${socket.id}`);
+});
